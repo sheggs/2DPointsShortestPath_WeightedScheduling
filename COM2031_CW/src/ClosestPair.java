ce93036ff9e095e0edc32916d2b530761f8759b0
unknown <46614077+sheggs@users.noreply.github.com>
2019-11-07 09:55:10 +0000

    Fixed Closest Path Java

--- COM2031_CW/src/ClosestPair.java
+++ COM2031_CW/src/ClosestPair.java
@@ -38,7 +38,7 @@ public class ClosestPair {
 	public double calculateDistance(Point a, Point b) {
 		System.out.println("( " + a.getX() + "- "+ b.getX() + ") ^ 2 + " + " ( " + a.getY() + " - " + b.getY() + ") ^ 2");
 		//Math.sqrt(((abs(a.getX()) - abs(b.getX()))*(abs(a.getX()) - abs(b.getX()))) - ((abs(a.getY()) - abs(b.getY()))*(abs(a.getY()) - abs(b.getY()))));
-		double calculation = ((Math.abs(a.X()) - Math.abs(b.X()))*(Math.abs(a.X()) - Math.abs(b.X()))) + ((Math.abs(a.Y()) - Math.abs(b.Y()))*(Math.abs(a.Y()) - Math.abs(b.Y())));
+		double calculation = ((a.X()) - (b.X()))*((a.X()) - (b.X())) + (((a.Y()) - (b.Y()))*((a.Y()) - (b.Y())));
 		System.out.println("hi");
 		System.out.println("CALC: " + calculation);
 		return Math.sqrt(calculation);
@@ -47,14 +47,14 @@ public class ClosestPair {
 		double min = Double.MAX_VALUE;
 		System.out.println("LEN " + points.length);
 		System.out.println(points[0]);
+		double dist = 0;
 		for(int i = 0; i<points.length;i++) {
-			for(int j = 1; j<points.length;j++) {
+			for(int j = i + 1; j<points.length;j++) {
 				System.out.println("SKR SKR");
-				double dist = calculateDistance(points[i], points[j]);
-				if(dist == 0) {
-					break;
-				}
+				dist = calculateDistance(points[i], points[j]);
 				min = min(min, dist);
+
+
 			}
 		}
 		return min;
@@ -66,10 +66,12 @@ public class ClosestPair {
 		}
 		System.out.println("mid" + middle + "len " + points.length);
 		// AINT WORKING FOR 4 LL  
+		
 		Point[] aux = Arrays.copyOfRange(points, 0, middle );
 		System.out.println(aux.length);
-		Point[] aux2 = Arrays.copyOfRange(points, middle + 1, points.length );
-
+	
+		Point[] aux2 = Arrays.copyOfRange(points, middle , points.length );
+		System.out.println(aux2.length);
 		double d1 = calcClosestPair(aux);
 		double d2 = calcClosestPair(aux2);
 		System.out.println(d1 + " " + d2);
@@ -77,11 +79,13 @@ public class ClosestPair {
 		// Only gets Delta
 		return d;
 	}
+	// {-3.0,-6.0},{-6.0,-5.0},{-5.0,3.0},{1.0,5.0},{-4.0,-1.0},{5.0,6.0},{-1.0,4.0},{6.0,-7.0}
 	public double min(double d1, double d2) {
 		return (d1<d2) ? d1:d2;
 	}
 	public static void main(String args[]) {
-		ClosestPair p = new ClosestPair(new Point[] {new Point(-6.0, -5.0), new Point(-5.0, 3.0),new Point(-4.0, -1.0),new Point (-3.0, -6.0)} );
+		ClosestPair p = new ClosestPair(new Point[] {new Point(-3.0,-6.0),new Point(-6.0,-5.0),new Point(-5.0,3.0),new Point(1.0,5.0),new Point(-4.0,-1.0),new Point(5.0,6.0),new Point(-1.0,4.0),new Point(6.0,-7.0)} );
+		
 		for(int i = 0; i< p.getPoints().length;i++) {
 			System.out.print("(" + p.getPoints()[i].getX() + "," + p.getPoints()[i].getY() + ")");
 		}
